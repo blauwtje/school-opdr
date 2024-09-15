8915bd4df80fb9243c8ba9a7ca35488ecfb43e97
blauwtje <1078121@hr.nl>
2024-09-15 22:23:46 +0200

    eerste push

---
+++ week2/W02.1.2O03/Player.cs
@@ -0,0 +1,12 @@
+public class Player
+{
+    //Your magic here
+    public string? Name;
+    public int HealthPoints = 100;
+    public int Power;
+    public Player(string? name, int power)
+    {
+        Name = name;
+        Power = power;
+    }
+}
---
+++ week2/W02.1.2O03/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Player p1 = ...;
+        Player p2 = ...;
+        Player winner = ...;
+        Console.WriteLine($"{p1.Name}: {p1.Power} Power; {p1.HealthPoints} Healthpoints");
+        Console.WriteLine($"{p2.Name}: {p2.Power} Power; {p2.HealthPoints} Healthpoints");
+        while (...)
+        {
+            p1.TakeDamage(...);
+            p2.TakeDamage(...);
+            if (...)
+                winner = p2;
+            else if (...)
+                winner = p1;
+            Console.WriteLine($"{p1.Name}: {p1.Power} Power; {p1.HealthPoints} Healthpoints");
+            Console.WriteLine($"{p2.Name}: {p2.Power} Power; {p2.HealthPoints} Healthpoints");
+        }
+
+        Console.WriteLine($"-----The winner is:-----\n{winner.Name}: {winner.Power} Power; {winner.HealthPoints} Healthpoints");
+    }
+}
