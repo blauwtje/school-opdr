0ccb80ba6b69d8eee8b021ea2085b1148330e122
blauwtje <1078121@hr.nl>
2024-09-15 22:50:34 +0200

    deel opdr 4

---
+++ week2/W02.1.2O04/Book.cs
@@ -0,0 +1,14 @@
+class Book
+{
+    public int ID;
+    public string? Title;
+    public Book(int id, string title)
+    {
+        ID = id;
+        Title = title;
+    }
+    public static string Info()
+    {
+
+    }
+}
