da94b8b9c919d80fc0a820b8a37414ca1820ed83
blauwtje <1078121@hr.nl>
2024-09-15 22:45:19 +0200

    opdr 3 af

--- week2/W02.1.2O03/Player.cs
+++ week2/W02.1.2O03/Player.cs
@@ -1,12 +1,23 @@
+
 public class Player
 {
     //Your magic here
     public string? Name;
     public int HealthPoints = 100;
     public int Power;
+
     public Player(string? name, int power)
     {
         Name = name;
         Power = power;
+    }
+    public bool IsAlive() => HealthPoints > 0;
+    public void TakeDamage(int damage)
+    {
+        HealthPoints -= damage;
+        if (HealthPoints < 0)
+        {
+            HealthPoints = 0;
+        }
     }
 }
--- week2/W02.1.2O03/Program.cs
+++ week2/W02.1.2O03/Program.cs
@@ -2,18 +2,18 @@
 {
     public static void Main()
     {
-        Player p1 = ...;
-        Player p2 = ...;
-        Player winner = ...;
+        Player p1 = new Player("John Snow", 30);
+        Player p2 = new Player("Night King", 60);
+        Player? winner = null;
         Console.WriteLine($"{p1.Name}: {p1.Power} Power; {p1.HealthPoints} Healthpoints");
         Console.WriteLine($"{p2.Name}: {p2.Power} Power; {p2.HealthPoints} Healthpoints");
-        while (...)
+        while (winner == null)
         {
-            p1.TakeDamage(...);
-            p2.TakeDamage(...);
-            if (...)
+            p1.TakeDamage(p2.Power);
+            p2.TakeDamage(p1.Power);
+            if (p1.HealthPoints == 0)
                 winner = p2;
-            else if (...)
+            else if (p2.HealthPoints == 0)
                 winner = p1;
             Console.WriteLine($"{p1.Name}: {p1.Power} Power; {p1.HealthPoints} Healthpoints");
             Console.WriteLine($"{p2.Name}: {p2.Power} Power; {p2.HealthPoints} Healthpoints");
