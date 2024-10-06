e6b0dd7cb59423e56226c8577e490c133599989a
blauwtje <1078121@hr.nl>
2024-10-06 09:53:16 +0200

    laatste opdr week3

--- week3/W03.1.1T06/Program.cs
+++ week3/W03.1.1T06/Program.cs
@@ -1,11 +1,1 @@
-﻿class Program
-{
-    static void Main()
-    {
-        const int spdOfLightInMperS = 299792458;
-        const double daysInYear = 365.242199;
-        const string planetName = "LP 890-9b";
-        const double distanceFromEarthInLightYear = 100;
-        Console.WriteLine($"The planet {planetName} is {distanceFromEarthInLightYear} light years away from Earth.\nIn meters this is {checked((long)spdOfLightInMperS * 60 * 60 * 24 * daysInYear * distanceFromEarthInLightYear)}");
-    }
-}
+﻿
